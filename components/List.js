332363244072460ba39fe1b7cdebfd5d38666142
Aapelii <aapeli.vaisanen@gmail.com>
2023-09-10 19:43:50 +0300

    Hello

---
+++ components/List.js
@@ -0,0 +1,34 @@
+import {FlatList} from 'react-native';
+import ListItem from './ListItem';
+import {useEffect, useState} from 'react';
+
+const url =
+  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+
+const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      //console.log(json);
+      setMediaArray(json);
+    } catch (error) {
+      console.error('loadMedia failed', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  return (
+    <FlatList
+      data={mediaArray}
+      renderItem={({item}) => <ListItem singleMedia={item} />}
+    />
+  );
+};
+
+export default List;
