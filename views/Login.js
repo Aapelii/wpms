472ac92c5718cedbfcec92d5c6ff59cf8dcff646
Aapelii <aapeli.vaisanen@gmail.com>
2023-09-10 22:27:26 +0300

    Hello

---
+++ views/Login.js
@@ -0,0 +1,60 @@
+import React, {useContext, useEffect} from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useUser} from '../hooks/ApiHooks';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
+
+const Login = ({navigation}) => {
+  // props is needed for navigation
+  const {setIsLoggedIn, setUser} = useContext(MainContext);
+  const {getUserByToken} = useUser();
+
+  const checkToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      // hardcoded token validation
+      const userData = await getUserByToken(token);
+      console.log('token', token);
+      console.log('userdata', userData);
+      if (userData) {
+        setIsLoggedIn(true);
+        setUser(userData);
+      }
+    } catch (error) {
+      console.log('checkToken', error);
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  return (
+    <TouchableOpacity
+      onPress={() => Keyboard.dismiss()}
+      style={{flex: 1}}
+      activeOpacity={1}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <LoginForm />
+        <RegisterForm />
+      </KeyboardAvoidingView>
+    </TouchableOpacity>
+  );
+};
+
+Login.propTypes = {
+  navigation: PropTypes.object,
+};
+
+export default Login;
---
+++ views/Single.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {mediaUrl} from '../utils/app-config';
+import {formatDate} from '../utils/functions';
+import {Card, Icon, Text, ListItem} from '@rneui/themed';
+
+const Single = ({route, navigation}) => {
+  // console.log('route params', route.params);
+  const {
+    title,
+    description,
+    filename,
+    time_added: timeAdded,
+    user_id: userId,
+    filesize,
+  } = route.params;
+  // Show full image and metadata
+  return (
+    <Card>
+      <Card.Title>{title}</Card.Title>
+      <Card.Image
+        source={{uri: mediaUrl + filename}}
+        resizeMode="center"
+        style={{height: 300}}
+      />
+      <ListItem>
+        <Text>{description}</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="save" />
+        <Text>{Math.round(filesize / 1024)} kB</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="today" />
+        <Text>{formatDate(timeAdded)}</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="person" />
+        <Text>id: {userId}</Text>
+      </ListItem>
+    </Card>
+  );
+};
+
+Single.propTypes = {
+  navigation: PropTypes.object,
+  route: PropTypes.object,
+};
+
+export default Single;
