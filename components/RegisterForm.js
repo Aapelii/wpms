d37e3403062829d7701e6fc1d7299bedaa048503
Aapelii <aapeli.vaisanen@gmail.com>
2023-09-10 22:24:48 +0300

    Hello

---
+++ components/RegisterForm.js
@@ -0,0 +1,12 @@
+import {Card, Text} from '@rneui/themed';
+
+const RegisterForm = () => {
+  return (
+    <Card>
+      <Card.Title>Register Form</Card.Title>
+      <Text>This was homework!</Text>
+    </Card>
+  );
+};
+
+export default RegisterForm;
